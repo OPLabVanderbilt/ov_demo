44ed729d5a11db534bce527bee69a8124f14fe21
JasonKChow <jason.k.chow@vanderbilt.edu>
2022-04-26 16:03:16 -0500

    trial file

---
+++ trials.js
@@ -0,0 +1,133 @@
+const blastTrials = [
+    {
+        "Image1": "blast1-1.png",
+        "Image2": "blast1-2.png",
+        "Image3": "blast1-3.png",
+        "Correct": 1
+    },
+    {
+        "Image1": "blast2-1.png",
+        "Image2": "blast2-2.png",
+        "Image3": "blast2-3.png",
+        "Correct": 3
+    },
+    {
+        "Image1": "blast3-1.png",
+        "Image2": "blast3-2.png",
+        "Image3": "blast3-3.png",
+        "Correct": 2
+    },
+    {
+        "Image1": "blast4-1.png",
+        "Image2": "blast4-2.png",
+        "Image3": "blast4-3.png",
+        "Correct": 3
+    }
+]
+
+const greebleTrials = [
+    {
+        "Image1": "greeble1-1.png",
+        "Image2": "greeble1-2.png",
+        "Image3": "greeble1-3.png",
+        "Correct": 2
+    },
+    {
+        "Image1": "greeble2-1.png",
+        "Image2": "greeble2-2.png",
+        "Image3": "greeble2-3.png",
+        "Correct": 1
+    },
+    {
+        "Image1": "greeble3-1.png",
+        "Image2": "greeble3-2.png",
+        "Image3": "greeble3-3.png",
+        "Correct": 2
+    },
+    {
+        "Image1": "greeble4-1.png",
+        "Image2": "greeble4-2.png",
+        "Image3": "greeble4-3.png",
+        "Correct": 1
+    }
+]
+
+const planeTrials = [
+    {
+        "StudyImage": "planeStudy1.png",
+        "Image1": "plane1-1.png",
+        "Image2": "plane1-2.png",
+        "Image3": "plane1-3.png",
+        "Correct": 3
+    },
+    {
+        "StudyImage": "planeStudy2.png",
+        "Image1": "plane2-1.png",
+        "Image2": "plane2-2.png",
+        "Image3": "plane2-3.png",
+        "Correct": 2
+    },
+    {
+        "StudyImage": "planeStudy3.png",
+        "Image1": "plane3-1.png",
+        "Image2": "plane3-2.png",
+        "Image3": "plane3-3.png",
+        "Correct": 2
+    },
+    {
+        "StudyImage": "planeStudy4.png",
+        "Image1": "plane4-1.png",
+        "Image2": "plane4-2.png",
+        "Image3": "plane4-3.png",
+        "Correct": 1
+    }
+]
+
+const foodTrials = [
+    {
+        "Image1": "food1-1.png",
+        "Image2": "food1-2.png",
+        "Image3": "food1-3.png",
+        "Image4": "food1-4.png",
+        "Correct": 4
+    },
+    {
+        "Image1": "food2-1.png",
+        "Image2": "food2-2.png",
+        "Image3": "food2-3.png",
+        "Image4": "food2-4.png",
+        "Correct": 3
+    },
+    {
+        "Image1": "food3-1.png",
+        "Image2": "food3-2.png",
+        "Image3": "food3-3.png",
+        "Image4": "food3-4.png",
+        "Correct": 1
+    },
+    {
+        "Image1": "food4-1.png",
+        "Image2": "food4-2.png",
+        "Image3": "food4-3.png",
+        "Image4": "food4-4.png",
+        "Correct": 4
+    }
+]
+
+const transformerTrials = [
+    {
+        "StudyImage": "transformerStudy1.png",
+        "Correct": 4,
+        "Close": [3, 5]
+    },
+    {
+        "StudyImage": "transformerStudy2.png",
+        "Correct": 2,
+        "Close": [1, 3]
+    },
+    {
+        "StudyImage": "transformerStudy3.png",
+        "Correct": 1,
+        "Close": [2, 6]
+    }
+]
